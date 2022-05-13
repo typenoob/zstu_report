0ab6fdfe689ec530b5da8b41106189a7e3ad96f6
typenoob <61347081+typenoob@users.noreply.github.com>
2022-05-13 17:12:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def success(name):
 def login():
     xh = request.form['xh']
     mm = request.form['mm']
-    dq = request.form['dq'] if request.form['cs'] else '浙江省 杭州市 钱塘区'
-    tz = request.form['tz'] if request.form['cs'] else ''
+    dq = request.form['dq'] if request.form['dq'] else '浙江省 杭州市 钱塘区'
+    tz = request.form['tz'] if request.form['tz'] else ''
     cs = request.form['cs'] if request.form['cs'] else '0'
     with open('./essentials.json', 'r', encoding='utf-8') as f:
         essentials = json.load(f)
